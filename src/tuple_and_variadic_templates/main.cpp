c3a6f95f051e6df1af1f0b32cef881ff5e30eab8
Bluemi <bruno.schilling@protonmail.ch>
2019-10-29 10:37:52 +0100

    added tuple_and_variadic_template example

--- src/tuple_and_variadic_templates/main.cpp
+++ src/tuple_and_variadic_templates/main.cpp
@@ -2,20 +2,48 @@
 #include <tuple>
 
 
-// Iterate tuple
+// ####################### Iterate tuple by members #######################
 
 template<size_t I = 0, typename ...Ts>
-std::enable_if_t<I == sizeof...(Ts)> iterate_tuple(const std::tuple<Ts...>&) {}
+std::enable_if_t<I == sizeof...(Ts)> print_tuple(const std::tuple<Ts...>&) {}
 
 
 template<size_t I = 0, typename ...Ts>
-std::enable_if_t<I < sizeof...(Ts)> iterate_tuple(const std::tuple<Ts...>& tuple) {
+std::enable_if_t<I < sizeof...(Ts)> print_tuple(const std::tuple<Ts...>& tuple) {
 	std::cout << std::get<I>(tuple) << std::endl;
-	iterate_tuple<I+1, Ts...>(tuple);
+	print_tuple<I+1, Ts...>(tuple);
 }
 
 
+// ####################### Iterate tuple by size #######################
+template<typename T>
+T inc_member(const T& t) {
+	return t + 1;
+}
+
+template<size_t ...I, typename ...Ts>
+std::tuple<Ts...> inc_tuple_impl(
+	[[maybe_unused]] std::index_sequence<I...>,
+	const std::tuple<Ts...>& t
+) {
+	return std::make_tuple(inc_member(std::get<I>(t)) ...);
+}
+
+template<typename ...Ts>
+std::tuple<Ts...> inc_tuple(const std::tuple<Ts...>& t) {
+	constexpr size_t tuple_size = std::tuple_size<std::tuple<Ts...>>::value;
+	return inc_tuple_impl(std::make_index_sequence<tuple_size>(), t);
+}
+
 int main() {
-	std::tuple<int, float> t = std::make_tuple(42, 3.1415f);
-	iterate_tuple(t);
+	using t_type = std::tuple<int, float, double, unsigned int>;
+	t_type t = std::make_tuple(42, 3.1415f, 54.0, 5);
+
+	std::cout << "created tuple: " << std::endl;
+	print_tuple(t);
+
+	std::cout << "incremented elements by one:" << std::endl;
+	t_type t_inc = inc_tuple(t);
+	print_tuple(t_inc);
+
 }
